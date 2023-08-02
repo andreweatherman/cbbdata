90f88590fb89cf9b46393953ec002e0027bc6f59
Andrew Weatherman <63006926+andreweatherman@users.noreply.github.com>
2023-08-02 10:16:26 -0400

    fix `torvik_player_game` function syntax

--- R/cbd_torvik_player_game.R
+++ R/cbd_torvik_player_game.R
@@ -3,6 +3,8 @@ cbd_torvik_player_game <- function() {
 
   base_url <- 'https://www.cbbdata.com/api/torvik/player/game?'
 
-  cbbdata:::get_cbd_file(base_url, ...)
+  data <- cbbdata:::get_cbd_file(base_url, ...)
+
+  return(data)
 
 }
