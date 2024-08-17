bd2bfb2eb605ce0508141897f114cae60bca4fbc
Andrew Weatherman <63006926+andreweatherman@users.noreply.github.com>
2024-08-16 20:39:20 -0400

    add ap rankings endpoint

--- R/cbd_ap_rankings.R
+++ R/cbd_ap_rankings.R
@@ -1,16 +1,16 @@
-#' Daily NET Rankings
+#' AP Poll History
 #'
-#' Pulls daily NET rankings along with other metrics from the 2023-24 season.
+#' Pulls AP poll history from 1949-Present
 #'
 #' @param ... OPTIONAL. To load the complete data set, pass no arguments through
 #'   to the function. Else, you can filter on  `team`, `conf`, or `year`.
 #'
 #' @examples
-#' \donttest{try(cbd_net_archive(team = "Duke"))}
+#' \donttest{try(cbd_ap_polls(team = "Duke"))}
 #' @export
-cbd_net_archive <- function(...) {
+cbd_ap_rankings <- function(...) {
 
-  base_url <- 'https://www.cbbdata.com/api/rankings/net?'
+  base_url <- 'https://www.cbbdata.com/api/rankings/ap?'
 
   data <- cbbdata:::get_cbd_file(base_url, ...)
 
