641539dcca5d8128442e2fb7a9ae9f1ff42047f4
Andrew Weatherman <63006926+andreweatherman@users.noreply.github.com>
2024-08-20 19:40:48 -0400

    critical update to fix cbd_match_teams

--- R/cbd_match_teams.R
+++ R/cbd_match_teams.R
@@ -10,7 +10,7 @@
 cbd_match_teams <- function() {
 
   teams <- cbbdata::cbd_teams() %>%
-    dplyr::select(-c(long, lat, espn_logo, espn_dark_logo, logo, wordmark, color, alt_color)) %>%
+    dplyr::select(-c(arena_lon, arena_lat, espn_logo, espn_dark_logo, logo, wordmark, color, alt_color, primary_arena, capacity, arena_elevation, arena_elevation_rank))  %>%
     tidyr::pivot_longer(-common_team)
 
   team_matching <- teams %>% dplyr::pull('common_team') %>%
